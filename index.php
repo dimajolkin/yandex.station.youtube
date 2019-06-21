52770b14da2d1248061bdd11253995c454bfc3ef
Dima <delkin@dealersocket.com>
2019-06-21 17:44:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 include __DIR__ . '/vendor/autoload.php';
 
-
 function create_cookies(array $array)
 {
     $items = [];
@@ -13,20 +12,14 @@ function create_cookies(array $array)
     return implode('; ', $items);
 }
 
-/**
- * Session_id required
- */
 $cookiesArray = json_decode(file_get_contents('cookies.json'));
 $cookiesData = create_cookies($cookiesArray);
-
-
 $jar = new \GuzzleHttp\Cookie\CookieJar;
 $client = new \GuzzleHttp\Client([
     'cookies' => $jar,
 ]);
 
 $youtubeVideoId = 'pTOPL1Ncsjc';
-
 $data = [
     'msg' => [
         'player_id' => 'youtube',
@@ -46,11 +39,8 @@ $headers = [
     //'x-csrf-token' => '<token>',
 ];
 $body = json_encode($data);
-
 $request = new \GuzzleHttp\Psr7\Request('POST', 'https://yandex.ru/video/station', $headers, $body);
-
 $response = $client->send($request);
-
 var_dump($response->getReasonPhrase());
 echo $response->getStatusCode(); # 200
 echo $response->getHeaderLine('content-type'); # 'application/json; charset=utf8'
